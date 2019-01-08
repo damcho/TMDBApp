a03c1cdc9bdf5a9dca7df604c8cc78d4955f1522
Damian Modernell <damcho@gmail.com>
2019-01-08 13:45:55 -0300

    refactor tmdbs

--- TMDBApp/MovieModule/Manager/MovieManager.swift
+++ TMDBApp/MovieModule/Manager/MovieManager.swift
@@ -13,7 +13,6 @@ class MovieManager {
     var presenter:MoviesPresenter?
     var movies:Dictionary<String, MoviesContainer> = Dictionary()
     let apiConnector:DataConnector = TMDBAPIConnector.shared
-    let dbConnector:DataConnector = TMDBCoreDataConnector.shared
     let imageCache = NSCache<NSString, UIImage>()
     static let shared = MovieManager()
     
@@ -21,7 +20,6 @@ class MovieManager {
         if Reachability.isConnectedToNetwork() {
             self.requestMoviesFromAPI(searchParams: searchParams)
         } else if moviesAreInMemory(searchParams: searchParams){
-            //self.requestMoviesFromDB(searchParams: searchParams)
             self.presenter?.moviesFetchedWithSuccess(movieContainer: self.movies[searchParams.category.rawValue]!)
         } else {
             self.presenter?.moviesFetchFailed(error: NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey:"Resource not found"]))
@@ -32,22 +30,7 @@ class MovieManager {
         return self.movies.index(forKey: searchParams.category.rawValue) != nil &&
         searchParams.page <=  self.movies[searchParams.category.rawValue]!.currentPage
     }
-    /*
-    func requestMoviesFromDB(searchParams: SearchObject) {
-        let completionHandler = { [unowned self] (moviePage:MoviesContainer?, error:Error?) -> () in
-            if error == nil {
-                if moviePage!.movies.count > 0 {
-                    self.movies.updateValue(moviePage!, forKey: searchParams.category.rawValue)
-                }
-                self.presenter?.moviesFetchedWithSuccess(moviePage: moviePage!)
-            } else {
-                self.presenter?.moviesFetchFailed(error:error!)
-            }
-        }
-        TMDBCoreDataConnector.shared.getMovies(searchParams: searchParams, completion: completionHandler)
-    }
- 
- */
+   
     
     func requestMoviesFromAPI(searchParams: SearchObject) {
         let completionHandler = {[unowned self] (movieContainer:MoviesContainer?, error:Error?) -> () in
@@ -56,15 +39,8 @@ class MovieManager {
                     self.movies[searchParams.category.rawValue] = movieContainer
                 } else {
                     self.movies[searchParams.category.rawValue]?.update(page: movieContainer!)
+                }
 
-                }
-                /*
-                do {
-                    try TMDBCoreDataConnector.shared.storeMovies(movies:self.movies[searchParams.filter.rawValue]!.movies)
-                } catch let error {
-                    self.presenter?.moviesFetchFailed(error:error)
-                }
- */
                 self.presenter?.moviesFetchedWithSuccess(movieContainer: self.movies[searchParams.category.rawValue]!)
             } else {
                 self.presenter?.moviesFetchFailed(error:error!)
@@ -84,14 +60,6 @@ class MovieManager {
         let apiDownloadedImageHandler = {[unowned self] (image:UIImage?) in
             if image != nil {
                 self.imageCache.setObject(image!, forKey: path as NSString)
-        //        self.dbConnector.saveImage(imageData: image!.pngData()!, with: path, and: nil)
-            }
-            completion(image)
-        }
-        
-        let dbDownloadedImageHandler = { [unowned self] (image:UIImage?) in
-            if image != nil {
-                self.imageCache.setObject(image!, forKey: path as NSString)
             }
             completion(image)
         }
@@ -99,7 +67,7 @@ class MovieManager {
         if Reachability.isConnectedToNetwork() {
             self.apiConnector.loadImage(from:path, completion:apiDownloadedImageHandler)
         } else {
-        //    self.dbConnector.loadImage(from: path, completion:dbDownloadedImageHandler)
+            completion(nil)
         }
     }
 }
