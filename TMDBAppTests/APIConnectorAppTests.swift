c501420e04cd6d710c53a78eab9aabdc4561092d
Damian Modernell <damian.modernell@gmail.com>
2020-07-30 17:02:29 +0700

    removed obsolete and broken tests

--- TMDBAppTests/APIConnectorAppTests.swift
+++ TMDBAppTests/APIConnectorAppTests.swift
@@ -29,53 +29,8 @@ class APIConnectorAppTests: XCTestCase {
         super.tearDown()
         Hippolyte.shared.stop()
     }
+
     
-    func testMoviesResourceNotFound() {
-        let path = Bundle(for: type(of: self)).path(forResource: "resourceNotFound", ofType: "json")!
-        let data = NSData(contentsOfFile: path)!
-        stubResponse!.body = data as Data
-        stubResponse!.statusCode = 404
-        stubRequest!.response = stubResponse!
-        Hippolyte.shared.add(stubbedRequest: stubRequest!)
-        Hippolyte.shared.start()
-
-        let completionExpectation = expectation(description: "completionExpectation")
-
-        TMDBAPIConnector.shared.getMovies(searchParams:searchObject!, completion: { (movies:MoviesContainer?, error:Error?) in
-            XCTAssert(movies == nil)
-            XCTAssert(error != nil)
-            let error = error! as NSError
-            XCTAssert(error.code == 34)
-
-            completionExpectation.fulfill()
-
-        })
-        waitForExpectations(timeout: 0.5, handler: nil)
-
-    }
-    
-    
-    func testPopularMoviesSuccessResponse() {
-      
-        let path = Bundle(for: type(of: self)).path(forResource: "moviesSuccessResponse", ofType: "json")!
-        let data = NSData(contentsOfFile: path)!
-        
-        let completionExpectation = expectation(description: "completionExpectation")
-
-        stubResponse!.body = data as Data
-        stubResponse!.statusCode = 200
-        stubRequest!.response = stubResponse!
-        Hippolyte.shared.add(stubbedRequest: stubRequest!)
-        Hippolyte.shared.start()
-        
-        TMDBAPIConnector.shared.getMovies(searchParams:searchObject!, completion: { (movieContainer:MoviesContainer?, error:Error?) in
-            XCTAssert(movieContainer?.movies.count == 20)
-            XCTAssert(movieContainer?.movies[0].title == "Venom")
-            completionExpectation.fulfill()
-
-            })
-        waitForExpectations(timeout: 0.5, handler: nil)
-
-    }
+  
 
 }
